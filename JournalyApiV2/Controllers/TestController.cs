0b36da35fce05e16f6de62e0f94e89dd830aea34
Gradyn Wursten <me@gradyn.com>
2023-10-17 11:43:35 -0600

    add test endpoint

---
+++ JournalyApiV2/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JournalyApiV2.Controllers;
+
+[Controller]
+[Route("/test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return StatusCode(204);
+    }
+}
