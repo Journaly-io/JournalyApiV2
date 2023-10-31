09947b89c86554683e889bfa0ea77837cbc0be9b
Gradyn Wursten <me@gradyn.com>
2023-10-31 10:46:57 -0600

    migrations

---
+++ JournalyApiV2/Data/Migrations/20231031025719_VerificationCodes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace JournalyApiV2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class VerificationCodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmailVerificationCode",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    VerificationCode = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    User = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailVerificationCode", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailVerificationCode");
+        }
+    }
+}
---
+++ JournalyApiV2/Data/Migrations/20231031161837_VerificationCodes2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JournalyApiV2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class VerificationCodes2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VerificationCode",
+                table: "EmailVerificationCode",
+                newName: "ShortCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LongCode",
+                table: "EmailVerificationCode",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LongCode",
+                table: "EmailVerificationCode");
+
+            migrationBuilder.RenameColumn(
+                name: "ShortCode",
+                table: "EmailVerificationCode",
+                newName: "VerificationCode");
+        }
+    }
+}
