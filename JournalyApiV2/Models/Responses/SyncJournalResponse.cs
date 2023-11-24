bf34d215d476cd1863e2024c940c1af630176b7d
Gradyn Wursten <me@gradyn.com>
2023-11-24 15:17:41 -0700

    batched syncs

--- JournalyApiV2/Models/Responses/SyncJournalResponse.cs
+++ JournalyApiV2/Models/Responses/SyncJournalResponse.cs
@@ -6,4 +6,5 @@ public class SyncJournalResponse
     public Emotion[] Emotions { get; set; } = Array.Empty<Emotion>();
     public Activity[] Activities { get; set; } = Array.Empty<Activity>();
     public EmotionCategory[] EmotionCategories { get; set; } = Array.Empty<EmotionCategory>();
+    public int Remaining { get; set; }
 }
