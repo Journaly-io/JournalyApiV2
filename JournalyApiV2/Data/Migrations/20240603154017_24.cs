a9cd0afee7ce0cb6d64286872a997c49b0172a0e
Gradyn Wursten <me@gradyn.com>
2024-06-03 09:40:46 -0600

    fix key store table

---
+++ JournalyApiV2/Data/Migrations/20240603154017_24.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JournalyApiV2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _24 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DEK",
+                table: "EncryptedDEK",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DEK",
+                table: "EncryptedDEK");
+        }
+    }
+}
