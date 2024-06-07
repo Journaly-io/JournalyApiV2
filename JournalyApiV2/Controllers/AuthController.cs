6c812080ffcc14ff14976f6cabf5511b9f189788
Gradyn Wursten <me@gradyn.com>
2024-06-07 09:27:11 -0600

    wrong exception lol

--- JournalyApiV2/Controllers/AuthController.cs
+++ JournalyApiV2/Controllers/AuthController.cs
@@ -225,9 +225,9 @@ public class AuthController : JournalyControllerBase
             {
                 Token = await _authService.IssueRecoveryTokenWithLongCode(request.LongCode)
             });
-        if (string.IsNullOrEmpty(request.ShortCode)) throw new BadRequestException("No recovery code provided");
+        if (string.IsNullOrEmpty(request.ShortCode)) throw new HttpBadRequestException("No recovery code provided");
         if (string.IsNullOrEmpty(request.Email))
-            throw new BadHttpRequestException("Short code provided but no email provided where it is required");
+            throw new HttpBadRequestException("Short code provided but no email provided where it is required");
         return new JsonResult(new AuthenticationResponse
         {
             Token = await _authService.IssueRecoveryTokenWithShortCode(request.Email, request.ShortCode)
